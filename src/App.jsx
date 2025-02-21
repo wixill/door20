e6c30bce9909ad629bb6b30d72b7ba7bacdd9e81
Wixill <wkjames8@gmail.com>
2025-02-21 15:36:50 -0500

    adding pdf images, adding pdf page, added materialUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import "./fonts/fonts.css";
-import './App.css'
+import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home";
 import AboutPage from "./pages/about";
 import CampaignPage from "./pages/campaign";
 import AdminPage from "./pages/admin";
+import PdfPage from "./pages/pdf";
 import NavBar from "./components/TopNav";
 import { useState, useEffect } from "react";
 
 function App() {
-
     function handlePageLoad() {
         window.scrollTo(0, 0);
     }
@@ -19,11 +19,48 @@ function App() {
             <NavBar />
             <div>
                 <Routes>
-                    <Route path="/" element={<HomePage onPageLoad={() => handlePageLoad()} />}  />
-                    <Route path="/about" element={<AboutPage onPageLoad={() => handlePageLoad()} />}  />
-                    <Route path="/moonseekers" element={<CampaignPage campaignName="Moonseekers" onPageLoad={() => handlePageLoad()} />} />
-                    <Route path="/skwad" element={<CampaignPage campaignName="Skwad" onPageLoad={() => handlePageLoad()} />}  />
-                    <Route path="/20admin" element={<AdminPage onPageLoad={() => handlePageLoad()} />} />
+                    <Route
+                        path="/"
+                        element={
+                            <HomePage onPageLoad={() => handlePageLoad()} />
+                        }
+                    />
+                    <Route
+                        path="/about"
+                        element={
+                            <AboutPage onPageLoad={() => handlePageLoad()} />
+                        }
+                    />
+                    <Route
+                        path="/moonseekers"
+                        element={
+                            <CampaignPage
+                                campaignName="Moonseekers"
+                                onPageLoad={() => handlePageLoad()}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/skwad"
+                        element={
+                            <CampaignPage
+                                campaignName="Skwad"
+                                onPageLoad={() => handlePageLoad()}
+                            />
+                        }
+                    />
+                    <Route
+                        path="/bloomburrow-players-guide"
+                        element={
+                            <PdfPage onPageLoad={() => handlePageLoad()} />
+                        }
+                    />
+                    <Route
+                        path="/20admin"
+                        element={
+                            <AdminPage onPageLoad={() => handlePageLoad()} />
+                        }
+                    />
                 </Routes>
             </div>
         </Router>
---
+++ src/pages/pdf.jsx
@@ -0,0 +1,45 @@
+import { useEffect, useRef, useState } from "react";
+import "../styles/pdf.less";
+import NavigateBeforeIcon from "@mui/icons-material/NavigateBefore";
+import NavigateNextIcon from "@mui/icons-material/NavigateNext";
+
+function PdfPage({ onPageLoad }) {
+    const pdfImages = useRef(
+        Object.keys(
+            import.meta.glob(
+                "/public/assets/pdf_images/bloomburrow_player_guide/*"
+            )
+        )
+    );
+
+    const [currentImage, setCurrentImage] = useState(0);
+
+    useEffect(() => {
+        onPageLoad();
+    }, []);
+
+    return (
+        <div className="page-pdf">
+            <button
+                onClick={() => setCurrentImage(currentImage - 1)}
+                className="pdf-prev-button"
+                disabled={currentImage === 0}
+            >
+                <NavigateBeforeIcon />
+            </button>
+            <img
+                className="pdf-image"
+                src={pdfImages.current[currentImage].replace("/public", "")}
+            />
+            <button
+                onClick={() => setCurrentImage(currentImage + 1)}
+                className="pdf-next-button"
+                disabled={currentImage === pdfImages.current.length - 1}
+            >
+                <NavigateNextIcon />
+            </button>
+        </div>
+    );
+}
+
+export default PdfPage;
